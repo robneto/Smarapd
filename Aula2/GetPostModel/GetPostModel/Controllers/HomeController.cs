f53ff190f1a169a1fe004fe66cb9917dc5e0abcc
robneto <robneto.stz@gmail.com>
2016-01-18 20:17:07 -0200

    Aulas
    Aulas

---
+++ Aula2/GetPostModel/GetPostModel/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using GetPostModel.Models;
+
+namespace GetPostModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa();
+
+            pessoa.PessoaId = 1;
+            pessoa.Nome = "Robson Batista Neto";
+            pessoa.Twitter = "robneto@hotmail.com";            
+
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+    }
+}
